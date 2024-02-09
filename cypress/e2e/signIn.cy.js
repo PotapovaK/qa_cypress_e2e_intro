05dfcb340323763145b9bd172fe4dec9dcb46f71
PotapovaK <karinapotapova8@gmail.com>
2024-02-09 16:29:17 +0200

    User Log In

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,18 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
+describe('Sign In user flow', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('https://react-redux.realworld.io/#/login?_k=0yd0kr');
+    cy.get('h1').should('contain.text', 'Sign In');
 
+    cy.get('[placeholder=Email]')
+      .type('karinapotapova8+10@gmail.com');
+
+    cy.get('[placeholder=Password]')
+      .type('passWord99');
+
+    cy.get('.btn').click();
+
+    cy.get(':nth-child(4) > .nav-link').should('contain.text', 'PK_test_user');
   });
 });
